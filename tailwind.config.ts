77002d1528a754cdaff9116c372f430496694879
John Dykes <98johndykes@gmail.com>
2023-10-31 09:29:19 -0400

    remove unnecessary screen sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -7,29 +7,34 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+      },
+    },
     extend: {
       fontFamily: {
         sans: ['var(--font-inter)'],
         mono: ['var(--font-source-code-pro)'],
       },
       colors: {
-        "background-color": "var(--background-color)",
-        "body-text-color": "var(--body-text-color)",
-        "highlight-color": "var(--highlight-color)",
-        "white-color": "var(--white-color)",
-        "secondary-highlight-color": "var(--secondary-highlight-color)",
-        "body-highlight-color": "var(--body-highlight-color)",
-        "neutral-color": "var(--neutral-color)",
-        "skillsButtonFillColor": "var(--skillsButtonFillColor)",
-        "skillsButtonTextColor": "var(--skillsButtonTextColor)",
+        'background-color': 'var(--background-color)',
+        'body-text-color': 'var(--body-text-color)',
+        'highlight-color': 'var(--highlight-color)',
+        'white-color': 'var(--white-color)',
+        'secondary-highlight-color': 'var(--secondary-highlight-color)',
+        'body-highlight-color': 'var(--body-highlight-color)',
+        'neutral-color': 'var(--neutral-color)',
+        skillsButtonFillColor: 'var(--skillsButtonFillColor)',
+        skillsButtonTextColor: 'var(--skillsButtonTextColor)',
       },
       letterSpacing: {
-        h3: '0.20em'
-      }
-    }
+        h3: '0.20em',
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
-export default config
+  plugins: [require('@tailwindcss/typography')],
+};
+export default config;
