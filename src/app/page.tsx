73ce98c08db733b9d356942026dc0abad669f68e
John Dykes <98johndykes@gmail.com>
2023-11-02 10:45:15 -0400

    add more experience cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -245,6 +245,7 @@ export default function Home() {
                   skills={[
                     'Geocomputation',
                     'Python',
+                    'R',
                     'Statistics',
                     'Machine Learning',
                   ]}
@@ -252,6 +253,25 @@ export default function Home() {
                   date='2019 — 2020'
                   title='Computer Research Programmer'
                   description='Worked on a team applying machine learning and Geocomputation to telecommunications data, including estimating the interference that cellular towers have on each other.'
+                ></ExperienceCard>
+                <ExperienceCard
+                  company='University of Waterloo'
+                  title='Master of Mathematics'
+                  date='Jan — Dec 2018'
+                  description="For my master's degree in Pure Mathematics, I worked with Dr. Yu-Ru Liu from the University of Waterloo. Studied Waring’s problem in Number Theory, in particular using Vinogradov’s Mean Value Theorem."
+                  skills={[
+                    'Mathematics',
+                    'Number Theory',
+                    'Mathematica',
+                    'Pathon/SageMath',
+                  ]}
+                ></ExperienceCard>
+                <ExperienceCard
+                  company='Carleton University'
+                  date='2014 — 2017'
+                  title='Bachelore of Mathematics, Honours'
+                  description="After taking two courses at Carleton University during the summer while being homeschooled and receiving A+s in both, I was accepted into the math program in the fall of 2018. I graduated with high distinction in December 2017. I made use of my Python programming skills during a Dean Summer Research Internship (DSRI) scholarship with Dr. Yuly Billig and during my Honour's project with Dr. Brett Stevens."
+                  skills={['Mathematics', 'MATLAB', 'Python']}
                 ></ExperienceCard>
               </div>
               <div className='mt-16 flex justify-end'>
