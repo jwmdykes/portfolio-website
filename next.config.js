7008299c962f3f1007cddb6a70d160a328949db2
John Dykes <98johndykes@gmail.com>
2023-11-09 20:26:52 -0500

    Add build id to filenames to avoid cache issues.

--- next.config.js
+++ next.config.js
@@ -3,7 +3,21 @@ const nextConfig = {
     output: 'export',
     images: {
         unoptimized: true
-    }
+    },
+    webpack: (config, { buildId, dev }) => {
+        const newConfig = config;
+
+        if (!dev && newConfig.output.filename.startsWith('static')) {
+            newConfig.output.filename = newConfig.output.filename.replace('[name]', `[name]-${buildId}`);
+            newConfig.output.chunkFilename = newConfig.output.chunkFilename.replace('[name]', `[name]-${buildId}`);
+        }
+
+        return newConfig;
+    },
+    generateBuildId: async () => {
+        const timestamp = Math.floor(Date.now() / 1000);
+        return `build-${timestamp}`;
+    },
 }
 
 module.exports = nextConfig
