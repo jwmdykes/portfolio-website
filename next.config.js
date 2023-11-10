7e368a245fdf33b7b6741872b8e44c202f332f30
John Dykes <98johndykes@gmail.com>
2023-11-09 20:42:44 -0500

    remove build id trick

--- next.config.js
+++ next.config.js
@@ -4,20 +4,6 @@ const nextConfig = {
     images: {
         unoptimized: true
     },
-    webpack: (config, { buildId, dev }) => {
-        const newConfig = config;
-
-        if (!dev && newConfig.output.filename.startsWith('static')) {
-            newConfig.output.filename = newConfig.output.filename.replace('[name]', `[name]-${buildId}`);
-            newConfig.output.chunkFilename = newConfig.output.chunkFilename.replace('[name]', `[name]-${buildId}`);
-        }
-
-        return newConfig;
-    },
-    generateBuildId: async () => {
-        const timestamp = Math.floor(Date.now() / 1000);
-        return `build-${timestamp}`;
-    },
 }
 
 module.exports = nextConfig
