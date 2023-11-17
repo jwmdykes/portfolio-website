3b55ee37f6a0bfd35663ea3c40d09ac99b53cd66
John Dykes <98johndykes@gmail.com>
2023-11-17 09:56:29 -0500

    modify image thumbnails

--- src/app/Sections/Projects.tsx
+++ src/app/Sections/Projects.tsx
@@ -8,14 +8,14 @@ export default function Projects() {
             <h3>Projects</h3>
             <div className='flex flex-col gap-20'>
                 <ProjectCard href={'https://medium.com/@98johndykes/creating-a-popup-dictionary-with-electron-part-1-7736c5e99670'}
-                    thumbnail='/images/electron-popup-dict-github.webp'
+                    thumbnail='/images/popupdict-project-thumbnail.png'
                     title='Creating a Popup Dictionary with Electron'
                     description='A popup dictionary created in electron for easy access to a dictionary while reading books, browsing the web or any other task. Since it is built in electron it works in any application, and it is extensible to any language since it is based on online dictionaries.'
                     alt='Thumbnail for Popup Dictionary Project'
                     skills={['Electron', 'Javascript', 'HTML/CSS', 'Python']}
                 ></ProjectCard>
                 <ProjectCard href={'https://medium.com/@98johndykes/reverse-engineering-encryption-of-a-korean-ebook-app-197d96b24c96'}
-                    thumbnail='/images/korean-ebook-reader-project.webp'
+                    thumbnail='/images/ebook-project-thumbnail.png'
                     title='Reverse Engineering a Korean eBook app'
                     description='Reverse engineering the DRM of an electron-based Korean eBook app. The purpose of this project was to be able to read my Korean eBooks on my kindle reader.'
                     alt='Thumbnail for Korean eBook Project'
